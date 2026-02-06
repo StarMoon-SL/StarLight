10492630ab4d4a97d5fb64296c09bb73b7a83f0e
StarMoon <2549007745@qq.com>
2026-02-06 20:17:50 +0800

    Update StarLight Loader.lua

--- StarLight Loader.lua
+++ StarLight Loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/StarMoon-SL/StarLight/refs/heads/main/StarLight/"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/StarMoon-SL/StarLight/refs/heads/main"))()
