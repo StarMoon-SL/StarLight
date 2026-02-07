7b733f3123685b0d0f96250eebcf5d6612de5c19
StarMoon <2549007745@qq.com>
2026-02-07 09:17:30 +0800

    Update StarLight Loader.lua

--- StarLight/StarLight Loader.lua
+++ StarLight/StarLight Loader.lua
@@ -61,4 +61,4 @@ if not validated then
     return
 end
 
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/.../download"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/dc15b5edf02bae8de414c6de43570d863cf3cf9b20b621c1d2c40270ed558aa2/download"))()
